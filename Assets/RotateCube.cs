44d88a5f5f93b525d819539814c4407969c14da4
Moosan <washer2016@outlook.jp>
2020-04-18 13:39:35 +0900

    AddProjject

---
+++ Assets/RotateCube.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour {
+
+    private Rigidbody _rigidBody;
+	void Start () {
+		_rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        _rigidBody.AddTorque(Vector3.right,ForceMode.Acceleration);
+	}
+}
---
+++ Assets/WebCamTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class WebCamTest : MonoBehaviour
+{
+
+    public MeshRenderer _renderer;
+
+    WebCamTexture webCamTexture;
+    WebCamDevice[] webCamDevice;
+    
+
+    void Start()
+    { 
+        webCamDevice = WebCamTexture.devices;
+        webCamTexture = new WebCamTexture(webCamDevice[0].name);
+        //webCamTexture.requestedHeight = 960;
+        //webCamTexture.requestedWidth = 1280;
+        _renderer.material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+    }
+}
